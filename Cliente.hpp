a48c0a97705c66017b0bfc8893b06dc95104b8e9
AyrtonSoler <victorayrtonphotography@gmail.com>
2024-11-21 06:38:59 -0600

    Creacion de metodos en Cliente.hpp, falta solucionar error en Tarjeta tarjeta (line 28).

--- Cliente.hpp
+++ Cliente.hpp
@@ -1,3 +1,29 @@
 //descripción: definición de la clase
 //fecha: nov, 20 - 2024
 //autores: Víctor Ayrton Urrutia & Franco De Escondrillas
+#pragma once
+using namespace std;
+#include <string>
+#include "Tarjeta.hpp"
+
+class Cliente {
+    public:
+        Cliente();
+        ~Cliente();
+        string getCliente();
+        string getNombre();
+        string getEmail();
+        string getPassword();
+        string getAddress();
+        void setName(string name);
+        void setEmail(string email);
+        void setPassword(string password);
+        void setAddress(string address);
+        void getData();
+    private:
+        string name;
+        string password;
+        string address;
+        string email;
+        Tarjeta tarjeta;
+};
